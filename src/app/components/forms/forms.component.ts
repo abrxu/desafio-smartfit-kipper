e54f8b543a3a634597da0e378ebd64e8a3eaf5ba
abrxu <abrxu@yahoo.com>
2025-06-20 03:18:33 -0300

    feat: added get units service

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { GetUnitsService } from '../../services/get-units.service';
 
 @Component({
   selector: 'app-forms',
@@ -16,11 +17,12 @@ export class FormsComponent implements OnInit {
   results = [];
   formGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService) {
 
   }
 
   ngOnInit(): void {
+    this.unitService.getAllUnits().subscribe(data => console.log(data));
     this.formGroup = this.formBuilder.group({
       hour: '',
       showClosed: false
